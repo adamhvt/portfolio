13a6993b3fa064c78a9b1b6f62f279731b4573b4
Adam Horvath <adam.horvath.0827@gmail.com>
2022-01-23 20:38:09 +0000

    Fixed build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+
+  Route, Routes
+} from "react-router-dom";
 import './App.scss';
+import Contact from './Components/Contact/Contact';
+import CV from './Components/CV/CV';
 import './Components/Header/Header';
 import Header from './Components/Header/Header';
 import HeaderMobile from './Components/HeaderMobile/HeaderMobile';
 import Introduction from './Components/Introduction/Introduction';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import CV from './Components/CV/CV';
-import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
