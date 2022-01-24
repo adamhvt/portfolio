cce6d4797080dbc4edbf8bed75e7c328d39af40c
Adam Horvath <adam.horvath.0827@gmail.com>
2022-01-24 14:33:10 +0000

    Updated text colors

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,3 @@
-import { faAt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import './Footer.scss';
 
@@ -9,7 +7,7 @@ function Footer() {
             <footer className="footer">
                 <div className="site-center">
                     <div className="contact">
-                        <FontAwesomeIcon icon={faAt} size="lg" />
+                        <span style={{ fontSize: "large" }}>@</span>
                         <a href="mailto:adam.horvath.0827@gmail.com">adam.horvath.0827@gmail.com</a>
                     </div>
                 </div>
