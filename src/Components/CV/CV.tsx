00848d9f045631b511a1e8ad804e70587ae3e282
Adam Horvath <adam.horvath.0827@gmail.com>
2022-02-05 19:47:11 +0000

    Added collapsible highlights

--- src/Components/CV/CV.tsx
+++ src/Components/CV/CV.tsx
@@ -1,11 +1,18 @@
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 // import doc from '../../Resources/Documents/Adam_Horvath_Resume_2022.pdf';
 import './CV.scss';
 
 function CV() {
+    const [highlightVisible, setHighlightVisible] = useState(Array.from({ length: 6 }, _ => false));
+
+    const showHighlight = (index: number) => {
+        highlightVisible[index] = true;
+        setHighlightVisible([...highlightVisible]);
+    };
+
     return (
         <>
             <div className="site-center">
@@ -34,23 +41,47 @@ function CV() {
                         <h4>Highlighted projects:</h4>
                         <p>
                             Automotive manufacturing quality test management and workflow system for one of the major automotive
-                            brands. Architected and led the development of a manufacturing support system using Angular, ASP.Net and
-                            MS SQL to implement complex user interfaces and workflows. Worked productively with clients to understand
-                            requirements and user needs, was able to keep balance between speed of delivery and quality while meeting
-                            high level security standards.
+                            brands.
+                            {
+                                !highlightVisible[0] ?
+                                    <a className="highlight-toggle" href={void (0)} onClick={() => showHighlight(0)}>More</a> :
+                                    <span>
+                                        <br />
+                                        Architected and led the development of a manufacturing support system using Angular, ASP.Net and
+                                        MS SQL to implement complex user interfaces and workflows. Worked productively with clients to understand
+                                        requirements and user needs, was able to keep balance between speed of delivery and quality while meeting
+                                        high level security standards.
+                                    </span>
+                            }
                         </p>
                         <p>
-                            Intranet portal for one of the major banks in Central Europe, the largest bank in Hungary. Developed several
-                            components to be integrated in SharePoint for creating, targeting and consuming news, recommendations
-                            and some social networking features using React with TypeScript and SPFx and continuous delivery. I was able
-                            to learn React in a very limited time and build UI inensive applications scalable on mobile, tablet and desktop
-                            devices. The system is used by thousands of users every day.
+                            Intranet portal for one of the major banks in Central Europe, the largest bank in Hungary.
+                            {
+                                !highlightVisible[1] ?
+                                    <a className="highlight-toggle" href={void (0)} onClick={() => showHighlight(1)}>More</a> :
+                                    <span>
+                                        <br />
+                                        Developed several
+                                        components to be integrated in SharePoint for creating, targeting and consuming news, recommendations
+                                        and some social networking features using React with TypeScript and SPFx and continuous delivery. I was able
+                                        to learn React in a very limited time and build UI inensive applications scalable on mobile, tablet and desktop
+                                        devices. The system is used by thousands of users every day.
+                                    </span>
+                            }
                         </p>
                         <p>
-                            Full-stack application framework to bolster delivering quality solutions on a fast pace: Responsibilities included
-                            architecting and leading the development of a front-end reusable component system using Angular by
-                            building multiple high quality components, services and modules. This framework has been used to deliver
-                            numerous solutions of small to larger scale.
+                            Full-stack application framework to bolster delivering quality solutions on a fast pace.
+                            {
+                                !highlightVisible[2] ?
+                                    <a className="highlight-toggle" href={void (0)} onClick={() => showHighlight(2)}>More</a> :
+                                    <span>
+                                        <br />
+                                        Responsibilities included
+                                        architecting and leading the development of a front-end reusable component system using Angular by
+                                        building multiple high quality components, services and modules. This framework has been used to deliver
+                                        numerous solutions of small to larger scale.
+                                    </span>
+                            }
                         </p>
                     </div>
 
@@ -67,23 +98,47 @@ function CV() {
                         </p>
                         <h4>Highlighted responsibilities and achievements:</h4>
                         <p>
-                            Product owner of a client facing content management and reporting system. I was responsible for
-                            managing feature and change requests for a system used for both managing sample files by clients and
-                            dynamically presenting research results.
+                            Product owner of a client facing content management and reporting system.
+                            {
+                                !highlightVisible[3] ?
+                                    <a className="highlight-toggle" href={void (0)} onClick={() => showHighlight(3)}>More</a> :
+                                    <span>
+                                        <br />
+                                        I was responsible for
+                                        managing feature and change requests for a system used for both managing sample files by clients and
+                                        dynamically presenting research results.
+                                    </span>
+                            }
                         </p>
                         <p>
-                            Product owner of a sample processing workflow system. I was responsible for implementing optimisations
-                            and new features to a system which was processing sample files at a high level of parallelism. Challenges
-                            included optimising the performance of running multiple thousand Azure Functions in parallel by writing
-                            optimal C# code and optimising database operations using CosmosDB and MS SQL.
+                            Product owner of a sample processing workflow system.
+                            {
+                                !highlightVisible[4] ?
+                                    <a className="highlight-toggle" href={void (0)} onClick={() => showHighlight(4)}>More</a> :
+                                    <span>
+                                        <br />
+                                        I was responsible for implementing optimisations
+                                        and new features to a system which was processing sample files at a high level of parallelism. Challenges
+                                        included optimising the performance of running multiple thousand Azure Functions in parallel by writing
+                                        optimal C# code and optimising database operations using CosmosDB and MS SQL.
+                                    </span>
+                            }
                         </p>
                         <p>
-                            Full software redesign of an internal project management system. The project resulted in significant
-                            performance imprevement for users, making them use the system more often which resulted in more up
-                            to date data regarding projects and field work. We also managed to improve code quality, maintainability
-                            and extendability by a large margin, due to designing a more consistent API, replacing existing design
-                            patterns with ones that better fit the purpose, removing/simplifying ones whiches added value at the cost
-                            of complexity was questionable and by making sure there is a clear separation of architecture layers.
+                            Full software redesign of an internal project management system.
+                            {
+                                !highlightVisible[5] ?
+                                    <a className="highlight-toggle" href={void (0)} onClick={() => showHighlight(5)}>More</a> :
+                                    <span>
+                                        <br />
+                                        The project resulted in significant
+                                        performance imprevement for users, making them use the system more often which resulted in more up
+                                        to date data regarding projects and field work. We also managed to improve code quality, maintainability
+                                        and extendability by a large margin, due to designing a more consistent API, replacing existing design
+                                        patterns with ones that better fit the purpose, removing/simplifying ones whiches added value at the cost
+                                        of complexity was questionable and by making sure there is a clear separation of architecture layers.
+                                    </span>
+                            }
                         </p>
                     </div>
                 </section>
